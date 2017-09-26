4a372987842999371f48da05eeeffc3b0bc6e111
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-09-26 19:30:18 +0100

    Setting Headers with Bootstrap and react-bootstrap

---
+++ src/Github.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Github extends Component {
+  render() {
+    return (
+      <div>
+        coming from github component;
+      </div>
+    )
+  }
+}
+
+export default Github;
